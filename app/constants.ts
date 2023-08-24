1fb302285ae326dbbc35dbc605c4b52f5ad80554
Naveen Yadav <naveeny8000@gmail.com>
2023-08-24 16:11:36 -0700

    Updated layout, added profile image

--- app/constants.ts
+++ app/constants.ts
@@ -1,46 +1,41 @@
-import { SectionEntry } from './types'
+import strings from './en.json'
 
-export const PREVIOUS_EMPLOYERS: SectionEntry[] = [
+export const SOCIALS: {
+  name: string
+  displayName: string
+  url: string
+}[] = [
   {
-    id: 0,
-    name: 'Cruise',
-    img: {
-      src: 'cruise.png',
-      height: 100,
-      width: 190,
-    },
-    url: 'https://getcruise.com/',
+    name: 'linkedin',
+    displayName: strings.linkedin,
+    url: 'https://www.linkedin.com/in/naveen-yadav8000',
   },
   {
-    id: 1,
-    name: 'Amazon Web Services',
-    img: {
-      src: 'aws.png',
-      height: 100,
-      width: 110,
-    },
-    url: 'https://aws.amazon.com/',
-  },
-  {
-    id: 2,
-    name: 'Groupon',
-    img: {
-      src: 'groupon.png',
-      height: 100,
-      width: 200,
-    },
-    url: 'https://groupon.com/',
-  },
-  {
-    id: 3,
-    name: 'Liberty Mutual',
-    img: {
-      src: 'lm.svg',
-      height: 100,
-      width: 250,
-    },
-    url: 'https://www.libertymutual.com/',
+    name: 'github',
+    displayName: strings.github,
+    url: 'https://github.com/nyadav810',
   },
 ]
 
-export const SKILLS: SectionEntry[] = []
+export const PREVIOUS_EMPLOYERS = [
+  'Cruise',
+  'Amazon Web Services',
+  'Groupon',
+  'Liberty Mutual Insurance',
+]
+
+export const SKILLS = [
+  'Full stack web development',
+  'Cloud development',
+  'Project management',
+  'Technical design',
+]
+
+export const PROFICIENCIES = [
+  'React',
+  'JavaScript',
+  'TypeScript',
+  'HTML',
+  'CSS',
+  'AWS',
+]
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,78 @@
-import { Section } from './components'
-import { PREVIOUS_EMPLOYERS, SKILLS } from './constants'
+import Image from 'next/image'
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col justify-between p-24">
-      <h1 className="text-6xl">Naveen Yadav</h1>
-      <Section
-        title="Previous Employers"
-        entries={PREVIOUS_EMPLOYERS}
-      ></Section>
-      <Section title="Skills" entries={SKILLS}></Section>
-      <Section title="Contact"></Section>
-    </main>
-  )
-}
+import strings from './en.json'
+import { PREVIOUS_EMPLOYERS, PROFICIENCIES, SKILLS, SOCIALS } from './constants'
+
+const Header = () => (
+  <div className="space-y-3 border-b-2 pb-4 mr-4">
+    <h1 className="text-6xl">{strings.title}</h1>
+    <h2 className="text-3xl text-gray-300 font-light">{strings.subtitle}</h2>
+    <div className="flex flex-row space-x-4">
+      {SOCIALS.map(({ name, displayName, url }) => (
+        <a
+          key={name}
+          href={url}
+          target="_blank"
+          className="text-blue-600 hover:underline hover:text-blue-300"
+        >
+          {displayName}
+        </a>
+      ))}
+    </div>
+  </div>
+)
+
+const ProfileIcon = () => (
+  <Image
+    className="self-end rounded-full"
+    src="/images/naveen_in_barcelona.png"
+    alt="Naveen in Barcelona"
+    height={400}
+    width={400}
+  ></Image>
+)
+
+const List = ({ title, list }: { title: string; list: string[] }) => (
+  <div className="basis-1/3">
+    <h4 className="font-bold mb-2">{title}</h4>
+    <ul className="list-disc pl-4">
+      {list.map((entry) => (
+        <li className="mb-2" key={entry}>
+          {entry}
+        </li>
+      ))}
+    </ul>
+  </div>
+)
+
+const Content = () => (
+  <section className="flex flex-row text-sm gap-3">
+    <List title={strings.previousEmployment} list={PREVIOUS_EMPLOYERS} />
+    <List title={strings.skills} list={SKILLS} />
+    <List title={strings.proficiencies} list={PROFICIENCIES} />
+  </section>
+)
+
+// const ContactForm = () => (
+//   <section>
+//     <h3 className="text-xl">{strings.contact}</h3>
+//     <form>
+//       <label>{strings.name}</label>
+//     </form>
+//   </section>
+// )
+
+const Home = () => (
+  <main className="flex min-h-screen flex-col space-y-6 pt-24">
+    <div className="flex flex-row">
+      <div className="flex flex-col grow space-y-5">
+        <Header />
+        <Content />
+      </div>
+      <ProfileIcon />
+    </div>
+    {/** TODO: Add Contact Form */}
+  </main>
+)
+
+export default Home
