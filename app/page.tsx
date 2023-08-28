08cc6f824e58018b816613ddb4a85c81587a05ee
Naveen Yadav <naveeny8000@gmail.com>
2023-08-28 15:41:17 -0700

    Add responsive styling

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,17 @@ import { PREVIOUS_EMPLOYERS, PROFICIENCIES, SKILLS, SOCIALS } from './constants'
 
 const Header = () => (
   <div className="space-y-3 border-b-2 pb-4 mr-4">
-    <h1 className="text-6xl">{strings.title}</h1>
-    <h2 className="text-3xl text-gray-400 font-light">{strings.subtitle}</h2>
+    <h1 className="text-4xl md:text-6xl">{strings.title}</h1>
+    <h2 className="text-2xl md:text-3xl text-gray-400 font-light">
+      {strings.subtitle}
+    </h2>
     <div className="flex flex-row space-x-4">
       {SOCIALS.map(({ name, displayName, url }) => (
         <a
           key={name}
           href={url}
           target="_blank"
-          className="text-blue-600 hover:underline hover:text-blue-300"
+          className="text-xl md:text-base text-blue-600 hover:underline hover:text-blue-300"
         >
           {displayName}
         </a>
@@ -22,7 +24,7 @@ const Header = () => (
 
 const ProfileIcon = () => (
   <img
-    className="self-end rounded-full"
+    className="self-end rounded-full mx-auto"
     src="https://s3.us-west-2.amazonaws.com/naveenyadav.me/naveen_in_barcelona.png"
     alt="Naveen in Barcelona"
     height={400}
@@ -44,7 +46,7 @@ const List = ({ title, list }: { title: string; list: string[] }) => (
 )
 
 const Content = () => (
-  <section className="flex flex-row text-sm gap-3">
+  <section className="text-xl gap-3 md:flex md:flex-row md:text-sm">
     <List title={strings.previousEmployment} list={PREVIOUS_EMPLOYERS} />
     <List title={strings.skills} list={SKILLS} />
     <List title={strings.proficiencies} list={PROFICIENCIES} />
@@ -61,9 +63,9 @@ const Content = () => (
 // )
 
 const Home = () => (
-  <main className="flex min-h-screen flex-col space-y-6 pt-24">
-    <div className="flex flex-row">
-      <div className="flex flex-col grow space-y-5">
+  <main className="min-h-screen p-8 md:p-0 md:pt-24">
+    <div className="flex flex-col-reverse space-y-6 space-y-reverse md:flex-row">
+      <div className="grow space-y-6">
         <Header />
         <Content />
       </div>
