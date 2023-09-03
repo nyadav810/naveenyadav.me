6bdb2e5176f152c3443c71a19b8149c1f3736a57
Naveen Yadav <naveeny8000@gmail.com>
2023-09-03 13:20:59 -0700

    Fix webp & png both loading bug

--- components/ProfileIcon.tsx
+++ components/ProfileIcon.tsx
@@ -1,17 +1,15 @@
-const S3_BUCKET_URL = 'https://s3.us-west-2.amazonaws.com/naveenyadav.me'
-const WEBP_SRC = `${S3_BUCKET_URL}/naveen_in_barcelona.webp`
-const PNG_SRC = `${S3_BUCKET_URL}/naveen_in_barcelona.png`
-
 const ProfileIcon = () => (
   <picture>
-    <source srcSet={WEBP_SRC} type="image/webp" />
+    <source
+      srcSet="https://s3.us-west-2.amazonaws.com/naveenyadav.me/naveen_in_barcelona.webp"
+      type="image/webp"
+    />
     <img
       className="self-end rounded-full mx-auto"
-      src={PNG_SRC}
+      src="https://s3.us-west-2.amazonaws.com/naveenyadav.me/naveen_in_barcelona.webp"
       alt="Naveen in Barcelona"
       height={400}
       width={400}
-      fetchPriority="high"
     />
   </picture>
 )
