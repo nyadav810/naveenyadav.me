15e9f205319651340608dd282c13b8f1c99a119a
Naveen Yadav <naveeny8000@gmail.com>
2023-09-03 11:29:47 -0700

    Add webp format profile image

--- components/ProfileIcon.tsx
+++ components/ProfileIcon.tsx
@@ -1,11 +1,18 @@
+const S3_BUCKET_URL = 'https://s3.us-west-2.amazonaws.com/naveenyadav.me'
+const WEBP_SRC = `${S3_BUCKET_URL}/naveen_in_barcelona.webp`
+const PNG_SRC = `${S3_BUCKET_URL}/naveen_in_barcelona.png`
+
 const ProfileIcon = () => (
-  <img
-    className="self-end rounded-full mx-auto"
-    src="https://s3.us-west-2.amazonaws.com/naveenyadav.me/naveen_in_barcelona.png"
-    alt="Naveen in Barcelona"
-    height={400}
-    width={400}
-  ></img>
+  <picture>
+    <source srcSet={WEBP_SRC} type="image/webp" />
+    <img
+      className="self-end rounded-full mx-auto"
+      src={PNG_SRC}
+      alt="Naveen in Barcelona"
+      height={400}
+      width={400}
+    />
+  </picture>
 )
 
 export default ProfileIcon
