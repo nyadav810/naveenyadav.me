79b8e993492747e3a71f49871c440120b49c6140
Naveen Yadav <naveeny8000@gmail.com>
2023-09-06 01:33:20 -0700

    Update title, description. Add OpenGraph tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,12 +5,27 @@ import 'app/globals.css'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
 
+const title =
+  'Freelance Software Developer | TypeScript, React, AWS | Naveen Yadav'
+const description =
+  'Freelance Software Developer with 9 years of experience. Use my expertise to supercharge your online business and customer experience.'
+
 export const metadata: Metadata = {
-  title: 'Naveen Yadav - Software Engineer For Hire',
-  description:
-    'Ex-Amazon Software Engineer for hire, specializing in web app development.',
+  title,
+  description,
   alternates: {
     canonical: 'https://naveenyadav.me',
+  },
+  openGraph: {
+    type: 'website',
+    url: 'https://naveenyadav.me',
+    title: 'naveenyadav.me',
+    description,
+    images: [
+      {
+        url: 'https://s3.us-west-2.amazonaws.com/naveenyadav.me/naveen_in_barcelona.png',
+      },
+    ],
   },
 }
 
