2fc320657c3d5bc52f6c30d04b419b93856966c6
Naveen Yadav <naveeny8000@gmail.com>
2023-09-06 00:54:15 -0700

    Fix profile icon fallback src

--- components/ProfileIcon.tsx
+++ components/ProfileIcon.tsx
@@ -6,7 +6,7 @@ const ProfileIcon = () => (
     />
     <img
       className="self-end rounded-full mx-auto"
-      src="https://s3.us-west-2.amazonaws.com/naveenyadav.me/naveen_in_barcelona.webp"
+      src="https://s3.us-west-2.amazonaws.com/naveenyadav.me/naveen_in_barcelona.png"
       alt="Naveen in Barcelona"
       height={400}
       width={400}
