7cfe1c8839c8715ffcc7fdea41d952549046828b
Naveen Yadav <naveeny8000@gmail.com>
2024-03-13 14:58:01 -0400

    Add contact form cypress test, cypress testing library, update eslint config

--- cypress/e2e/home_page.cy.ts
+++ cypress/e2e/home_page.cy.ts
@@ -1,7 +1,24 @@
+const AMPLIFY_CONSOLE_URL =
+  'https://us-east-1.console.aws.amazon.com/amplify/home?region=us-east-1#/'
+
 describe('The Home Page', () => {
-  it('successfully loads', () => {
+  it('should load successfully', () => {
     cy.visit('/')
 
     cy.contains('Naveen Yadav')
+  })
+
+  it('should submit the contact form successfully', () => {
+    cy.visit('/')
+
+    cy.findByRole('textbox', { name: /name/i }).type('Naveen Yadav')
+    cy.findByRole('textbox', { name: /e-mail/i }).type('n_yadav@comcast.net')
+    cy.findByRole('textbox', { name: /message/i }).type(
+      `This is a test message from Cypress! Find out if the tests passed: ${AMPLIFY_CONSOLE_URL}`
+    )
+
+    cy.findByRole('button').click()
+
+    cy.contains('Thank you! Your message has been sent.')
   })
 })
