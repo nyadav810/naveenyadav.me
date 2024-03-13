9f1f93f52be693fd5b116303d7d89de67a5bcad3
Naveen Yadav <naveeny8000@gmail.com>
2024-03-13 16:44:31 -0400

    chore: Add content security policy

--- next.config.js
+++ next.config.js
@@ -4,7 +4,36 @@ const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
 })
 
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' https://d3lhhs2b8t5fnk.cloudfront.net blob: data:;
+    font-src 'self';
+    object-src 'none';
+    connect-src 'self' https://7x38u4owyj.execute-api.us-east-1.amazonaws.com;
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    block-all-mixed-content;
+    upgrade-insecure-requests;
+`
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+        ],
+      },
+    ]
+  },
+}
 
 module.exports = withBundleAnalyzer(nextConfig)
