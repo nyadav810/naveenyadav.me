625b5bd46f7db462f4915e12a7ad77ac28dd0f97
Naveen Yadav <naveeny8000@gmail.com>
2024-03-29 16:41:05 -0400

    feat: Add Github, Linkedin icons, a11y labels

--- app/constants.ts
+++ app/constants.ts
@@ -1,18 +1,32 @@
 import strings from 'app/en.json'
+import { Github, Linkedin, LucideIcon, LucideProps } from 'lucide-react'
 
 export const SOCIALS: {
   name: string
-  displayName: string
+  a11yText: string
   url: string
+  bgColor: string
+  icon: LucideIcon
+  iconProps: LucideProps
 }[] = [
   {
     name: 'linkedin',
-    displayName: strings.linkedin,
+    a11yText: strings.linkedinLinkA11yText,
     url: 'https://www.linkedin.com/in/naveen-yadav8000',
+    bgColor: '#0a66c2',
+    icon: Linkedin,
+    iconProps: {
+      color: '#fff',
+    },
   },
   {
     name: 'github',
-    displayName: strings.github,
+    a11yText: strings.githubLinkA11yText,
     url: 'https://github.com/nyadav810',
+    bgColor: '#333',
+    icon: Github,
+    iconProps: {
+      color: '#fff',
+    },
   },
 ]
--- components/Header.tsx
+++ components/Header.tsx
@@ -2,20 +2,23 @@ import { SOCIALS } from 'app/constants'
 import strings from 'app/en.json'
 
 const Header = () => (
-  <header className="space-y-3 border-b-2 pb-4">
+  <header className="space-y-3 border-b-2 pb-3 md:pb-2">
     <h1>{strings.title}</h1>
     <h2 className="text-gray-400 font-light">{strings.subtitle}</h2>
-    <div className="flex flex-row space-x-4">
-      {SOCIALS.map(({ name, displayName, url }) => (
-        <a
-          key={name}
-          href={url}
-          target="_blank"
-          className="text-blue-600 hover:underline hover:text-blue-300"
-        >
-          {displayName}
-        </a>
-      ))}
+    <div className="flex gap-x-4 md:gap-x-2">
+      {SOCIALS.map(
+        ({ name, a11yText, bgColor, icon: Icon, url, iconProps }) => (
+          <span
+            key={name}
+            className={'border-2 p-2.5 md:p-1 rounded-full hover:opacity-75'}
+            style={{ backgroundColor: bgColor }}
+          >
+            <a aria-label={a11yText} href={url} target="_blank">
+              <Icon {...iconProps} />
+            </a>
+          </span>
+        )
+      )}
     </div>
   </header>
 )
